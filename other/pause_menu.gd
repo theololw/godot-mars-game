fcf537f089d223cf5106445830d9dff630ecde73
theololw <127053072+theololw@users.noreply.github.com>
2024-03-05 21:01:34 +0100

    expanded on pause menu
    added a resume button, quit button and two additional placeholder buttons to the pause screen

--- other/pause_menu.gd
+++ other/pause_menu.gd
@@ -6,10 +6,26 @@ var paused = false
 
 func _process(delta):
 	if Input.is_action_just_pressed("escape"):
-		paused = !paused
+		toggle_pause()
 	if paused == true:
 		get_tree().paused = true
 		pauseMenu.show()
 	else:
 		get_tree().paused = false
 		pauseMenu.hide()
+
+
+func _on_continue_pressed():
+	toggle_pause()
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+
+func toggle_pause():
+	paused = !paused
+
+
+
+
